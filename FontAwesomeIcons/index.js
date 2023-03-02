610bd2096d39437109602391ff8f880f2382c0d5
vtyacodes <vitiyah95@gmail.com>
2023-03-02 09:24:10 +0800

    Netflix clone project

---
+++ FontAwesomeIcons/index.js
@@ -0,0 +1,7 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { library } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/react-fontawesome";
+
+import { faCheckSquare } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCheckSquare);
